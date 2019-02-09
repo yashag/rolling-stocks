86c99b9e8fb0023b659c3c81d1e9449ac268eaef
yashag <yashagoonki@gmail.com>
2019-02-10 00:16:27 +0200

    Updates

---
+++ precache-manifest.be584fb81578370be0de970bbcfa7308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0373b58fb1847411afb4",
+    "url": "/rolling-stocks/static/css/main.37a85062.chunk.css"
+  },
+  {
+    "revision": "0373b58fb1847411afb4",
+    "url": "/rolling-stocks/static/js/main.0373b58f.chunk.js"
+  },
+  {
+    "revision": "bc23106ff3a41ac259b7",
+    "url": "/rolling-stocks/static/js/1.bc23106f.chunk.js"
+  },
+  {
+    "revision": "1c1dd4c04e4f608fe3a3",
+    "url": "/rolling-stocks/static/js/runtime~main.1c1dd4c0.js"
+  },
+  {
+    "revision": "fbaaba3a8112f736133fc0f62d86ecdf",
+    "url": "/rolling-stocks/static/media/logo.fbaaba3a.svg"
+  },
+  {
+    "revision": "0ca0005d0c7c7e4bd1296e63b8dab69b",
+    "url": "/rolling-stocks/index.html"
+  }
+];
